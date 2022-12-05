439e18b76d89ca319601a7f01e94799677b0de3c
tommybobbins <tng@chegwin.org>
2022-12-05 15:51:09 +0000

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,15 +2,15 @@ resource "aws_instance" "supersecret" {
   ami       = data.aws_ssm_parameter.ami_id.value
   subnet_id = module.vpc.public_subnets[0]
   #ipv6_cidr_block                 = cidrsubnet(module.vpc.ipv6_cidr_block, 8, 1)
-  instance_type   = "t3.nano"
-  security_groups = [aws_security_group.ec2-sg.id]
-  #user_data       = fileexists("userdata.sh") ? file("userdata.sh") : null
-  #user_data            = data.template_file.init.rendered
-  user_data            = base64encode(templatefile("userdata.sh", local.userdata))
-  iam_instance_profile = aws_iam_instance_profile.ec2_access_role.name
-  key_name             = aws_key_pair.generated_key.key_name
+  instance_type          = "t3.nano"
+  vpc_security_group_ids = [aws_security_group.ec2-sg.id]
+  #user_data              = fileexists("userdata.sh") ? file("userdata.sh") : null
+  #user_data              = data.template_file.init.rendered
+  user_data              = base64encode(templatefile("userdata.sh", local.userdata))
+  iam_instance_profile   = aws_iam_instance_profile.ec2_access_role.name
+  key_name               = aws_key_pair.generated_key.key_name
   tags = {
-    Name            = var.host_name
+    Name                 = var.host_name
   }
     root_block_device {
      volume_type = "gp3"
